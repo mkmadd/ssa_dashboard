7e0c46ed8f422e425a7ecc4936a3029344ae7c8e
Michael K. Maddeford <mkmadd@gmail.com>
2015-01-13 14:23:34 -0600

    Wrap up while moving to a new project

--- web_doc_test.py
+++ web_doc_test.py
@@ -19,10 +19,174 @@ DOCUMENT_NAME = 'SSA Doc' # google docs name
 username = getenv('GOOG_UID') # google/gmail login id
 passwd = getenv('GOOG_PWD') # google/gmail login password
 
-server = getenv('SSA_SERVER')
-db = getenv('SSA_DATABASE')
-uid = getenv('SSA_UID')
-pwd = getenv('SSA_PWD')
+
+EXPIRED_TIME = 17   # Number of minutes after which a time is expired
+
+# From levels Marc sent
+WARNING_LEVELS = {
+    'Shortstop 1' : {
+        'Unlead A, Unlead B' : 4128,
+        'Prem.' : 500,
+        'Diesel' : 716
+    },
+    'Shortstop 3' : {
+        'Unlead' : 1453,
+        'Premium' : 536,
+        'Diesel' : 500
+    },
+    'Shortstop 4' : {
+        'Unlead' : 3765,
+        'Premium' : 771,
+        'Diesel' : 3120
+    },
+    'Shortstop 8' : {
+        'Unleaded' : 4815,
+        'Premium' : 1012,
+        'Diesel' : 2117,
+        'Unlead WE' : 1404,
+        'Unlead WW' : 3500,
+        'Diesel West' : 742
+    },
+    'Shortstop 10' : {
+        'Unleaded' : 4028,
+        'Premium' : 790,
+        'Diesel' : 874
+    },
+    'Shortstop 12' : {
+        'Unleaded' : 4510,
+        'Premium' : 622,
+        'Diesel' : 657
+    },
+    'Shortstop 13' : {
+        'Unleaded' : 9506,
+        'Diesel' : 6622,
+        'Premium' : 1133,
+        'Def' : 1500
+    },
+    'Shortstop 16' : {
+        'Unleaded' : 4998,
+        'Premium' : 794,
+        'Diesel' : 707
+    },
+    'Shortstop 18' : {
+        'Unleaded' : 2782,
+        'Premium' : 633,
+        'Diesel' : 850
+    },
+    'Shortstop 20' : {
+        'Unleaded' : 6991,
+        'Premium' : 687,
+        'Diesel' : 890,
+        'Diesel LG' : 1235
+    },
+    'Shortstop 21' : {
+        'Unleaded' : 2429,
+        'Diesel' : 848
+    },
+    'Shortstop 22' : {
+        'Unleaded' : 2360,
+        'Premium' : 500,
+        'Diesel, Diesel' : 3766
+    },
+    'Shortstop 23' : {
+        'Unlead 1, Unlead 2' : 6972,
+        'Premium' : 803,
+        'Diesel' : 610
+    },
+    'Shortstop 24' : {
+        'Unleaded A, Unleaded B' : 4356,
+        'Premium' : 791,
+        'Diesel A, Diesel B' : 4142
+    },
+    'Shortstop 25' : {
+        'Unlead' : 2733,
+        'Premium' : 528,
+        'Diesel' : 3422
+    },
+    'Shortstop 26' : {
+        'Unleaded' : 2107,
+        'Premium' : 614,
+        'E-15' : 589,
+        'Diesel' : 768
+    }
+}
+
+
+def fix_name(name):
+    fixed_name = ''
+    for word in name.strip().split():
+        if len(word) > 2:
+            word = word.capitalize()
+        fixed_name += ' ' + word
+    return fixed_name.strip()
+
+# Format raw database output for consumption by index template
+# 0. SITE_ID
+# 1. STORAGE_ID
+# 2. STORAGE_TYPE_ID
+# 3. GROSS_VOLUME
+# 4. ULLAGE
+# 5. GROSS_WATER_VOLUME
+# 6. WATER_LEVEL
+# 7. LAST_UPDATED
+# 8. NAME (tank)
+# 9. PRODUCT_NAME
+# 10. NAME (store)
+# TODO: fix - 11 - 14 added by add_levels(), currently not available
+# 11. high level
+# 12. low level
+# 13. delivery needed level
+# 14. water level limit
+def format_stores(rows):
+    stores = []
+    for name, tanks in groupby(rows, lambda x: x[10]):
+        store = {}
+        store['store_name'] = name
+        
+        tank_info = []
+        for tank in tanks:
+            new_tank = {}
+            new_tank['site_id'] = tank[0]
+            new_tank['storage_id'] = tank[1]
+            new_tank['tank_name'] = fix_name(tank[8])
+            new_tank['product_name'] = fix_name(tank[9])
+            new_tank['gross_volume'] = int(tank[3]) if tank[3] is not None else None
+            new_tank['ullage'] = int(tank[4]) if tank[4] is not None else None
+            new_tank['last_updated'] = tank[7]
+            new_tank['tank_low'] = False
+            # For the most part the manifolds have a different product name
+            # than the other tanks.  Shortstop 25 tanks 1, 2, and the manifold
+            # all have product name 'Unlead'
+            if name != 'Shortstop 25':
+                warn_lvl = WARNING_LEVELS[name].get(new_tank['product_name'], 0)
+            elif '1' not in new_tank['tank_name'] and \
+                 '2' not in new_tank['tank_name']:
+                warn_lvl = WARNING_LEVELS[name].get(new_tank['product_name'], 0)
+            else:
+                warn_lvl = 0
+            if new_tank['gross_volume'] <= warn_lvl:
+                new_tank['tank_low'] = True
+            tank_info.append(new_tank)
+            
+        # Find the row with the earliest time to use at the last update
+        earliest = min(tank_info, key=lambda x: x['last_updated'])
+        store['last_update_time'] = earliest['last_updated'].strftime('%I:%M %p')
+        store['last_update_date'] = earliest['last_updated'].strftime("%b %d, '%y")
+        
+        # If old date/time, set expired flags
+        if earliest['last_updated'].date() != datetime.today().date():
+            store['date_expired'] = True
+        else:
+            store['date_expired'] = False
+        if (earliest['last_updated'] + timedelta(minutes=EXPIRED_TIME)) < datetime.now():
+            store['time_expired'] = True
+        else:
+            store['time_expired'] = False
+            
+        store['tanks'] = tank_info
+        stores.append(store)
+    return stores
+
 
 def load_docs():
     client = gdata.docs.client.DocsClient(source='drive_test')
@@ -52,62 +216,23 @@ def retrieve_doc():
             sep_rows.append(temp)
     return sep_rows
 
-def fix_name(name):
-    fixed_name = ''
-    for word in name.strip().split():
-        if len(word) > 2:
-            word = word.capitalize()
-        fixed_name += ' ' + word
-    return fixed_name.strip()
 
-# TODO - This function was copied over from bottle_test.py and modified
-# Should do the modifications outside and keep a single function
-# Format raw database output for consumption by index template
-# 0. SITE_ID
-# 1. STORAGE_ID
-# 2. STORAGE_TYPE_ID
-# 3. GROSS_VOLUME
-# 4. ULLAGE
-# 5. GROSS_WATER_VOLUME
-# 6. LAST_UPDATED
-# 7. NAME (tank)
-# 8. PRODUCT_NAME
-# 9. NAME (store)
-def format_stores(rows):
-    sorted_rows = natsorted(rows, key=lambda x: x[-1])
-    stores = []
-    print rows[:5]; pass
-    for name, tanks in groupby(sorted_rows, lambda x: x[9]):
-        store = {}
-        store['store_name'] = name
-        
-        tank_info = []
-        for tank in tanks:
-            new_tank = {}
-            new_tank['tank_name'] = fix_name(tank[7])
-            new_tank['product_name'] = fix_name(tank[8])
-            new_tank['water_vol'] = float(tank[5])
-            max = float(tank[3]) + float(tank[4]) + float(tank[5])
-            new_tank['max_capacity'] = max
-            new_tank['capacity'] = (float(tank[3]) + float(tank[5])) / max
-            new_tank['last_updated'] = datetime.strptime(tank[6], '%Y-%m-%d %H:%M:%S')
-            tank_info.append(new_tank)
-        
-        # Find the row with the earliest time to use at the last update
-        earliest = min(tank_info, key=lambda x: x['last_updated'])
-        store['last_updated'] = earliest['last_updated'].strftime('%I:%M %p')
-        store['time_expired'] = True
-
-        store['tanks'] = tank_info
-        stores.append(store)
-    return stores
+#@route('/alarms')
+#@view('alarm')
+#def show_alarms():
+#    alarms = get_todays_active_alarms()
+#    alarms = natsorted(alarms, key=lambda x: x[0])
+#    #alarms = format_alarms(alarms)
+#    return { 'url': url, 'alarms': alarms}
+    
 
 @route('/')
 @view('index')
 def index():
     rows = retrieve_doc()
+    rows = natsorted(rows, key=lambda x: x[-1])
     stores = format_stores(rows)
-    return { 'url': url, 'stores': stores }
+    return { 'url': url, 'stores': stores, 'alarms': [None] }
 
 @route('/static/:path#.+#', name='static')
 def static(path):
@@ -115,6 +240,6 @@ def static(path):
 
 debug(True)     # For development use only
 # Reloader for development use only
-run(host='10.0.0.27', port=8080, reloader=True)
-#rows = retrieve_doc()
-#print rows[:5]
+#run(host='10.0.0.27', port=8080, reloader=True)
+rows = retrieve_doc()
+print rows[:5]
