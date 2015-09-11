fea7c00c7d67de6d5cc5af87803121a6af85ad85
Michael K. Maddeford <mkmadd@gmail.com>
2015-09-10 21:12:01 -0500

    Revert DST 'fix'

--- bottle_test.py
+++ bottle_test.py
@@ -134,32 +134,54 @@ def get_time_int(time):
 
 
 def get_latest_updates():
-    # Get numbers from LOG_INVENTORY, JOIN with STORAGE to get tank and product
-    # names, then JOIN with SITE to get store names, excluding 
-    # STORAGE_TYPE_ID 102, which doesn't show up in SSA website
-    test_query = """SELECT tank_names.*, SITE.NAME FROM 
-        (SELECT last.*, sto.NAME, sto.PRODUCT_NAME
-        FROM (SELECT I1.SITE_ID, I1.STORAGE_ID, I1.STORAGE_TYPE_ID, 
-        I1.GROSS_VOLUME, I1.ULLAGE, I1.GROSS_WATER_VOLUME, I1.WATER_LEVEL, 
-        I1.LAST_UPDATED
+    """Query database for latest inventory data.
+    
+    Get numbers from LOG_INVENTORY, JOIN with STORAGE to get tank and product
+    names, then JOIN with SITE to get store names, excluding 
+    STORAGE_TYPE_ID 102, which doesn't show up in SSA website
+    NOTE: Monitor update times do not seem to account for daylight savings,
+    have to add one hour when DST in effect.
+    
+    Returns: List of tuples containing site id, storage id, storage type id,
+        gross volume, ullage, gross water volume, water level, DST adjusted
+        last update datetime, tank name, product name, and store name.
+    """
+    test_query = """
+    SELECT  tank_names.SITE_ID, 
+            tank_names.STORAGE_ID, 
+            tank_names.STORAGE_TYPE_ID, 
+            tank_names.GROSS_VOLUME,
+            tank_names.ULLAGE, 
+            tank_names.GROSS_WATER_VOLUME,
+            tank_names.WATER_LEVEL, 
+            tank_names.LAST_UPDATED,
+            tank_names.NAME,
+            tank_names.PRODUCT_NAME, 
+            SITE.NAME
+    FROM (
+        SELECT last.*, sto.NAME, sto.PRODUCT_NAME
+        FROM (
+            SELECT I1.SITE_ID, I1.STORAGE_ID, I1.STORAGE_TYPE_ID, 
+                   I1.GROSS_VOLUME, I1.ULLAGE, I1.GROSS_WATER_VOLUME, I1.WATER_LEVEL, 
+                   I1.LAST_UPDATED
             FROM LOG_INVENTORY I1
             JOIN (
-                SELECT MAX(LAST_UPDATED) AS LAST_UPDATED, SITE_ID, 
-                STORAGE_ID, STORAGE_TYPE_ID
+                SELECT MAX(LAST_UPDATED) AS LAST_UPDATED,
+                    SITE_ID, STORAGE_ID, STORAGE_TYPE_ID
                 FROM LOG_INVENTORY
                 GROUP BY SITE_ID, STORAGE_ID, STORAGE_TYPE_ID) as I2
-                ON I1.SITE_ID = I2.SITE_ID AND
+            ON I1.SITE_ID = I2.SITE_ID AND
                 I1.STORAGE_ID = I2.STORAGE_ID AND
                 I1.STORAGE_TYPE_ID = I2.STORAGE_TYPE_ID AND
                 I1.LAST_UPDATED = I2.LAST_UPDATED
             WHERE I1.STORAGE_TYPE_ID <> 102) last
         RIGHT JOIN STORAGE sto
-        ON last.STORAGE_ID = sto.STORAGE_ID AND
-        last.STORAGE_TYPE_ID = sto.STORAGE_TYPE_ID AND
-        last.SITE_ID = sto.SITE_ID) tank_names
+        ON  last.STORAGE_ID = sto.STORAGE_ID AND
+            last.STORAGE_TYPE_ID = sto.STORAGE_TYPE_ID AND
+            last.SITE_ID = sto.SITE_ID) tank_names
         RIGHT JOIN SITE
         ON tank_names.SITE_ID = SITE.SITE_ID
-    ORDER BY tank_names.SITE_ID
+    ORDER BY tank_names.SITE_ID;
     """
     conn = pyodbc.connect(connection_string)
     cursor = conn.cursor()
@@ -173,26 +195,32 @@ def get_latest_updates():
 
     
 def get_todays_active_alarms():
+    """Query database for any active alarms.
+    
+    Returns: List of tuples containing site name, category, device, device
+    name, description, status, date time, and last updated
+    """
     query = """
         SELECT site.NAME, a.CAT_NAME, 
             CONCAT(a.DEV_NAME, ' ', a.DEV) as DEV_NAME, a.DEVICE_NAME, 
-            a.CODE_NAME, a.STAT_NAME, a.DATE_TIME, a.LAST_UPDATED FROM
-        	(SELECT b.*, stat.NAME as STAT_NAME FROM
-        		(SELECT z.*, devs.NAME as DEV_NAME FROM
-        			(SELECT y.*, cats.NAME as CAT_NAME FROM 
-        				(SELECT x.SITE_ID, codes.CATEGORY, codes.DEVICE, 
+            a.CODE_NAME, a.STAT_NAME, a.DATE_TIME, a.LAST_UPDATED
+        FROM
+            (SELECT b.*, stat.NAME as STAT_NAME FROM
+                (SELECT z.*, devs.NAME as DEV_NAME FROM
+                    (SELECT y.*, cats.NAME as CAT_NAME FROM 
+                        (SELECT x.SITE_ID, codes.CATEGORY, codes.DEVICE, 
                                  x.DEVICE as DEV, x.DEVICE_NAME, 
                                  codes.NAME as CODE_NAME, x.STATUS, 
                                  x.DATE_TIME, x.LAST_UPDATED
-        					FROM (SELECT SITE_ID, CODE, DATE_TIME, DEVICE,
-        						STATUS, LAST_UPDATED, DEVICE_NAME
-        						FROM LOG_ALARMS
-        						WHERE IS_ACTIVE = 1 AND CODE <> 7112 AND
-        						CONVERT(DATE, LAST_UPDATED) = CONVERT(DATE, GetDate())) x
-        					JOIN CONST_ALARM_CODES codes ON x.CODE = codes.CODE) y
-        			JOIN CONST_ALARM_CATEGORIES cats ON y.CATEGORY = cats.CODE) z
-        		JOIN CONST_ALARM_DEVICES devs ON z.DEVICE = devs.CODE) b
-        	JOIN CONST_ALARM_STATUS_LIST as stat ON b.STATUS = stat.CODE) a
+                            FROM (SELECT SITE_ID, CODE, DATE_TIME, DEVICE,
+                                STATUS, LAST_UPDATED, DEVICE_NAME
+                                FROM LOG_ALARMS
+                                WHERE IS_ACTIVE = 1 AND CODE <> 7112 AND
+                                CONVERT(DATE, LAST_UPDATED) = CONVERT(DATE, GetDate())) x
+                            JOIN CONST_ALARM_CODES codes ON x.CODE = codes.CODE) y
+                    JOIN CONST_ALARM_CATEGORIES cats ON y.CATEGORY = cats.CODE) z
+                JOIN CONST_ALARM_DEVICES devs ON z.DEVICE = devs.CODE) b
+            JOIN CONST_ALARM_STATUS_LIST as stat ON b.STATUS = stat.CODE) a
         JOIN SITE ON a.SITE_ID = SITE.SITE_ID
     """
     
@@ -361,7 +389,6 @@ def index():
     rows = get_latest_updates()
     rows = natsorted(rows, key=lambda x: x[-1])
     alarms = get_todays_active_alarms()
-#    rows = add_levels(rows)
     stores = format_stores(rows)
     return { 'url': url, 'stores': stores, 'alarms': alarms }
     
